2780a432d2267b3f430da97dce4da792190ff80c
Brian Crafton <crafton.b@husky.neu.edu>
2019-08-24 10:41:36 -0400

    test seems alright

--- LoadCOCO_test.py
+++ LoadCOCO_test.py
@@ -9,10 +9,21 @@ while True:
     if not loader.empty():
         image, (coords, objs, no_objs) = loader.pop()
 
+        print (np.shape(coords), np.shape(objs), np.shape(no_objs))
+
         for ii in range(len(coords)):
+
             coord = coords[ii]
-            idx = np.where(coord > 0)
-            [x, y, w, h, _] = coord[idx]
+            obj = objs[ii]
+
+            flag = False
+            for jj in range(7):
+                for kk in range(7):
+                    if obj[jj][kk]:
+                        assert(not flag)
+                        [x, y, w, h, _] = coord[jj][kk]
+                        flag = True
+            assert(flag)
 
             x = int(x)
             y = int(y)
