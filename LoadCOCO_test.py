95bc848741ea2feb64750db865913ff437739a8c
Brian Crafton <crafton.b@husky.neu.edu>
2019-08-23 23:41:41 -0400

    show image

--- LoadCOCO_test.py
+++ LoadCOCO_test.py
@@ -1,6 +1,11 @@
 
 from LoadCOCO import LoadCOCO
+import matplotlib.pyplot as plt
 
 loader = LoadCOCO()
 
-
+while True:
+    if not loader.empty():
+        image, (coords, obj, no_obj) = loader.pop()
+        plt.imshow(image)
+        plt.show()
