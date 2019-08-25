f7c558a5068015f1e90c44b232b725718834e1e8
Brian Crafton <crafton.b@husky.neu.edu>
2019-08-25 13:18:37 -0400

    draws boxes well

--- draw_boxes.py
+++ draw_boxes.py
@@ -14,10 +14,17 @@ def draw_boxes(name, image, predict):
     # top5 = np.argsort(conf)[-5:]
 
     [x, y, w, h, c] = np.squeeze(predict[top1])
+    # print (x, y, w, h)
+    x = int(x * 448.)
+    y = int(y * 448.)
+    w = int(w * 448.)
+    h = int(h * 448.)
+    # print (x, y, w, h)
 
     image = image / np.max(image)
     top = image
     bottom = np.copy(image)
+    bottom[x:(x+w), y:(y+h), :] = 0.0
     concat = np.concatenate((top, bottom), axis=1)
     plt.imsave(name, concat)
 
