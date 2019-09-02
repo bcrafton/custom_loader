f826d674cb6f23a1bd7ba8c86f728a55468ad681
Brian Crafton <crafton.b@husky.neu.edu>
2019-09-02 10:25:01 -0400

    good boxerS

--- LoadCOCO_test.py
+++ LoadCOCO_test.py
@@ -8,7 +8,7 @@ loader = LoadCOCO()
 counter = 0
 while True:
     if not loader.empty():
-        image, (coords, objs, no_objs) = loader.pop()
+        image, (coords, objs, no_objs, cats) = loader.pop()
         image = np.squeeze(image)
 
         for ii in range(len(coords)):
@@ -25,8 +25,18 @@ while True:
 
             image = image / np.max(image)
             top = image
+            '''
             bottom = np.copy(image)
             bottom[x:(x+w), y:(y+h), :] = 0.
+            '''
+            bottom = np.copy(image)
+            [x11, x12, x21, x22] = [x, x+5, x+w-5, x+w]
+            [y11, y12, y21, y22] = [y, y+5, y+h-5, y+h]
+            red = np.array([1.0, 0.0, 0.0])
+            bottom[x11:x12, y12:y21, :] = red
+            bottom[x21:x22, y12:y21, :] = red
+            bottom[x12:x21, y11:y12, :] = red
+            bottom[x12:x21, y21:y22, :] = red
             concat = np.concatenate((top, bottom), axis=1)
 
             plt.imsave('%d.jpg' % (counter), concat)
